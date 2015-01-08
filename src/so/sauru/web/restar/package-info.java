ece1e06ac27994f9e1ad208f4cbfca74ee65469d
Yonghwan SO <sio4@users.sf.net>
2015-01-08 11:29:53 +0900

    apply GPLv3 License

--- src/so/sauru/web/restar/package-info.java
+++ src/so/sauru/web/restar/package-info.java
@@ -1,6 +1,25 @@
-/**
+/*****************************************************************************
+ * RESTar, simple RestFul API framework.
+ *   Page: http://sr-web.github.io/restar
+ *   Source: https://github.com/sr-web/restar
+ *   Author: Yonghwan SO <sio4@users.sf.net>
  * 
- */
+ * Copyright (c) 2014-2015 Yonghwan SO <sio4@users.sf.net>
+ * 
+ * This file is part of RESTar.
+ * 
+ * RESTar is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * RESTar is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RESTar. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
 /**
  * @author sio4
  *
